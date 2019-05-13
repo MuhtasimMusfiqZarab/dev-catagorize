3d0b68094627231ad46ee0b2ac28a54afeb5ad80
zarab <musfiqzarab@iut-dhaka.edu>
2019-05-13 15:26:53 +0600

    Finalize

--- scripts/products-app.js
+++ scripts/products-app.js
@@ -47,5 +47,5 @@ window.addEventListener("storage", e => {
 if (document.referrer !== document.location.href) {
   setTimeout(function() {
     document.location.reload();
-  }, 1000);
+  }, 200);
 }
